9ef9a10d36003b3a8e8be024547879b584f6611b
paulo-torre <pauloricardo.torre@gmail.com>
2026-05-31 15:46:24 -0300

    fix: removendo rotas de docs

--- app/main.py
+++ app/main.py
@@ -4,19 +4,32 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, books, user
 
-app = FastAPI(title="API da Biblioteca Virtual")
-
 ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
 if ENVIRONMENT == "production":
     origins = [] # A definir!
+    allow_credentials = True
+    docs_url = None
+    redoc_url = None
+    openapi_url = None
 else:
-    origins = ["http://localhost:4200"]
+    origins = ["*"]
+    allow_credentials = False
+    docs_url = "/docs"
+    redoc_url = "/redoc"
+    openapi_url = "/openapi.json"
+
+app = FastAPI(
+    title="API da Biblioteca Virtual",
+    docs_url=docs_url,
+    redoc_url=redoc_url,
+    openapi_url=openapi_url
+)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=allow_credentials,
     allow_methods=["*"],
     allow_headers=["*"]
 )
