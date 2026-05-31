5c413f9d3ac9f6023bf4466e479a93fb4ab787ca
paulo-torre <pauloricardo.torre@gmail.com>
2026-05-31 15:47:32 -0300

    refactor: criação da classe AppSettings

--- app/main.py
+++ app/main.py
@@ -1,35 +1,61 @@
-import os
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import computed_field
+from pydantic_settings import BaseSettings
+
 from app.routers import auth, books, user
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
-if ENVIRONMENT == "production":
-    origins = [] # A definir!
-    allow_credentials = True
-    docs_url = None
-    redoc_url = None
-    openapi_url = None
-else:
-    origins = ["*"]
-    allow_credentials = False
-    docs_url = "/docs"
-    redoc_url = "/redoc"
-    openapi_url = "/openapi.json"
+class AppSettings(BaseSettings):
+    environment: str = "development"
+
+    @computed_field
+    @property
+    def is_production(self) -> bool:
+        return self.environment == "production"
+    
+    @computed_field
+    @property
+    def origins(self) -> list[str]:
+        # A DEFINIR O RETORNO EM PRODUCTION
+        return [] if self.is_production else ["*"]
+
+    @computed_field
+    @property
+    def allow_credentials(self) -> bool:
+        return self.is_production
+    
+    @computed_field
+    @property
+    def docs_url(self) -> str | None:
+        return None if self.is_production else "/docs"
+
+    @computed_field
+    @property
+    def redoc_url(self) -> str | None:
+        return None if self.is_production else "/redoc"
+    
+    @computed_field
+    @property
+    def openapi_url(self) -> str | None:
+        return None if self.is_production else "/openapi.json"
+    
+    class Config:
+        env_file = ".env"
+
+settings = AppSettings()
 
 app = FastAPI(
     title="API da Biblioteca Virtual",
-    docs_url=docs_url,
-    redoc_url=redoc_url,
-    openapi_url=openapi_url
+    docs_url=settings.docs_url,
+    redoc_url=settings.redoc_url,
+    openapi_url=settings.openapi_url
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=allow_credentials,
+    allow_origins=settings.origins,
+    allow_credentials=settings.allow_credentials,
     allow_methods=["*"],
     allow_headers=["*"]
 )
